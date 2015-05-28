4a1f82c6b8790b5b34452c17e6d175b2765370bc
Thomas Goldenberg <thomasagoldenberg@gmail.com>
2015-05-28 15:13:32 -0400

    add test method

--- chessboard.rb
+++ chessboard.rb
@@ -2,17 +2,26 @@ require 'pry'
 require_relative 'loadboard'
 require_relative 'piece'
 
+LETTERS = {
+  "a" => 1,
+  "b" => 2,
+  "c" => 3,
+  "d" => 4,
+  "e" => 5,
+  "f" => 6,
+  "g" => 7,
+  "h" => 8
+}
+
 class ChessBoard
   attr_accessor :board
   def initialize
     @board = {}
-
     (1..8).each do |x|
       (1..8).each do |y|
         @board["#{x.to_s}#{y.to_s}"] = []
       end
     end
-
     (21..28).each {|int| @board[int.to_s] = Pawn.new("black")}
     (71..78).each {|int| @board[int.to_s] = Pawn.new("white")}
     [11, 18].each {|int| @board[int.to_s] = Rook.new("black")}
@@ -32,6 +41,12 @@ class ChessBoard
       puts ("#{8 - index }| ") + arr.join(" | ") + "\n  _______________________________|\n"
     end
     puts                       "    a   b   c   d   e   f   g   h "
+  end
+
+  def test_method(start)
+    location = start.split
+    piece = @board[LETTERS[location.first].to_s + location.last]
+    puts piece
   end
 
 end
