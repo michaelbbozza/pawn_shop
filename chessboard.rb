8f013163bce429fc65c81a2b5b86ebe5a7030a9a
Thomas Goldenberg <thomasagoldenberg@gmail.com>
2015-05-28 14:05:20 -0400

    add board

--- chessboard.rb
+++ chessboard.rb
@@ -10,10 +10,21 @@ class ChessBoard
     end
     (21..28).each {|int| @board[int.to_s] = Pawn.new("black")}
     (71..78).each {|int| @board[int.to_s] = Pawn.new("white")}
-    @board
+    [11, 18].each {|int| @board[int.to_s] = Rook.new("black")}
+    [81,88].each {|int| @board[int.to_s] = Rook.new("white")}
+    [12, 17].each {|int| @board[int.to_s] = Knight.new("black")}
+    [82, 87].each {|int| @board[int.to_s] = Knight.new("white")}
+    [13, 16].each {|int| @board[int.to_s] = Bishop.new("black")}
+    [83, 86].each {|int| @board[int.to_s] = Bishop.new("white")}
+    @board[14.to_s] = Queen.new("black")
+    @board[15.to_s] = King.new("black")
+    @board[84.to_s] = King.new("white")
+    @board[85.to_s] = Queen.new("white")
+
+    p @board.each {|k,v| v}
   end
 end
 
 board = ChessBoard.new
+puts "8 ║♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜"
 
-p board
--- piece.rb
+++ piece.rb
@@ -12,16 +12,66 @@ class Pawn < Piece
 end
 
 class Bishop < Piece
+  def initialize(color)
+    @color = color
+    if @color == "white"
+        ## TODO
+    else
+      ## TODO
+    end
+  end
+
 end
 
 class Rook < Piece
+  def initialize(color)
+    @color = color
+    if @color == "white"
+        ## TODO
+    else
+      ## TODO
+    end
+  end
+
 end
 
 class Knight < Piece
+  def initialize(color)
+    @color = color
+    if @color == "white"
+        ## TODO
+    else
+      ## TODO
+    end
+  end
+
 end
 
 class Queen < Piece
+  def initialize(color)
+    @color = color
+    if @color == "white"
+        ## TODO
+    else
+      ## TODO
+    end
+  end
+
 end
 
 class King < Piece
+    def initialize(color)
+      @color = color
+      if @color == "white"
+          ## TODO
+      else
+        ## TODO
+      end
+    end
+
 end
+
+puts "8 ║♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜"
+puts "♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙"
+puts "♖ … ♗ ♕ ♔ ♗ ♘ ♖"
+puts "♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟"
