8abc5054abb978c34c41649716614b146de093c4
Michael Bozza <michaelbbozza@gmail.com>
2015-05-29 16:08:03 -0400

    got load and save working

--- controller.rb
+++ controller.rb
@@ -3,14 +3,14 @@ require_relative 'chessboard'
 require_relative 'piece'
 require_relative 'menu'
 require_relative 'Parser'
+require_relative 'hashmap'
 require 'pry'
 
 class Controller
+  attr_reader :game_board
   def initialize
     ControllerView.clear_terminal
-    # @game_board = ChessBoard.new
     Menu.start
-    # binding.pry
   end
 
   def self.start_game
@@ -20,11 +20,11 @@ class Controller
     input = ControllerView.begin
     if input == 'yes'
       sleep 0.5
-      run
+      run_new_game
     elsif input != 'no'
       input = ControllerView.reenter_option
       if input == 'yes'
-        run
+        run_new_game
       elsif input == 'no'
         ControllerView.goodbye
       end
@@ -36,25 +36,35 @@ class Controller
   end
 
   def self.resume_game
-  #   # @old_board = @game_board.dup
-  #   Parser.import('chess.csv')
-  #   ControllerView.print(@old_board.to_s)
+    @game_board = ChessBoard.new
+    @parsed_board =  Parser.import('chess.csv')
+    @game_board.board = @parsed_board
+    ControllerView.print(@game_board.to_s)
+    ControllerView.turn
+    ControllerView.rotate_player
+    run_new_game
   end
 
-  def self.run
-    2.times do
+  def self.run_new_game
+    20.times do
       source = ControllerView.get_user_piece
+      board_index = @game_board.number_converter(source)
+      piece_possible_moves = @game_board.board[board_index].possible_moves(board_index, @game_board.board)
+      ControllerView.give_possible_moves(piece_possible_moves)
       target = ControllerView.get_user_target
       sleep 0.4
       @game_board.move(source,target)
+      # @game_board.set_variables
+      # @game_board.in_check?(player)
       ControllerView.clear_terminal
       ControllerView.print(@game_board.to_s)
       ControllerView.turn
+      Parser.save(@game_board.board)
     end
     ControllerView.continue
     input = ControllerView.user_input
     if input == 'yes'
-      run
+      run_new_game
     elsif input == 'save'
       Parser.save(@game_board.board)
     elsif input == 'no' ||input == 'exit' || input == 'quit'
--- menu.rb
+++ menu.rb
@@ -1,4 +1,3 @@
-# require
 module Menu
 
   def self.start
